b2693f33c379b37071e65a3a8c979b98d8e2e7b1
justincoh <justincoh@gmail.com>
2014-12-02 12:42:51 -0500

    Pie Chart Slice Toggling

--- pieCharts/pieCharts.js
+++ pieCharts/pieCharts.js
@@ -54,7 +54,8 @@
 
 var width = 960,
     height = 500,
-    radius = Math.min(width, height) / 2;
+    radius = Math.min(width, height) / 2,
+    labelRadius = radius * 1.2;
 
 var color = d3.scale.category20()
     // .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
@@ -76,6 +77,8 @@ var data = [   //This was test data
  {age:'More Junk',population:50}
  ];
 
+
+
  var total=0; //for calculating percentages
  for(var i = 0; i<data.length;i++){
   total += data[i]['population'];
@@ -89,6 +92,8 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
     // console.log("SVG: ",Object.keys(svg[0][0]).sort())
+
+
 
   var g = svg.selectAll(".arc")
       .data(pie(data))
@@ -109,14 +114,19 @@ var svg = d3.select("body").append("svg")
       .style("text-anchor", "middle")
       .text(function(d) { return d.data.age; });
 
+  //For tooltip positioning
 
+  var element = d3.selectAll('svg')
+  element = element[0][0];
+  var bbox = element.getBBox();
+  
   var tooltip = d3.select('body').append('div')
               .style('position', 'absolute')
               .style('padding', '0 10px')
               .style('background', 'black')
               .style('color','white')
               .style('opacity', 0) // setting to 0 because we dont want it to show when the graphic first loads
-
+              .style('font-size','20px')
           d3.selectAll('path').on('click', function(d) {
             // console.log('D ',d)
             d3.select(this)
@@ -125,8 +135,11 @@ var svg = d3.select("body").append("svg")
               .style('opacity', .9)
             tooltip.html('Value '+ (d['value']/total).toFixed(2)+'%')
               // console.log(d)
-              .style('left', (d3.event.pageX -15) + 'px')
-              .style('top', (d3.event.pageY - 30) + 'px')
+              // .style('left', (d3.event.pageX -15) + 'px')
+              // .style('top', (d3.event.pageY - 30) + 'px')
+              .style('left', (bbox.width*.5) + 'px')
+              .style('top', (bbox.height/2) + 'px')
+                
           })
             .on('mouseout', function(d) {
                 d3.select(this)
@@ -134,16 +147,9 @@ var svg = d3.select("body").append("svg")
                 tooltip.transition()
                   .style('opacity', 0)
             })
-
-
-
-
-// });
 }
 
 
 
 
 createPieChart(data)
-
-// module.exports = createPieChart;
